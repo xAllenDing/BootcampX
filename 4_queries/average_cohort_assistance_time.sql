a982ea13a578c16be57afa018fbc87138fb27440
Allen Ding <allen.ding@hotmail.com>
2023-02-07 18:48:04 -0800

    add query for average assistance time for cohorts

---
+++ 4_queries/average_cohort_assistance_time.sql
@@ -0,0 +1,6 @@
+SELECT cohorts.name, avg(completed_at - started_at) as average_assistance_time
+FROM assistance_requests
+JOIN cohorts ON cohorts.id = cohort_id
+JOIN students ON student.id = student_id
+GROUP BY cohorts.name
+ORDER BY average_assistance_time;
